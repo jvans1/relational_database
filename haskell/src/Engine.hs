b67b9767c9f5fde0bc946890cb80c03b81bcb6d8
James Vanneman <jvannem@gmail.com>
2017-02-12 16:39:22 -0800

    WIP

--- haskell/src/Engine.hs
+++ haskell/src/Engine.hs
@@ -2,20 +2,17 @@
 module Engine (runQuery, queryParser, Filterable, filterByField) where
 import Data.Text(Text, isPrefixOf)
 
-type Table = Text
-type ColName = Text
-
-data Iterator = Filter Text Text Iterator | SeqScan [ deriving Show
+data Iterator = Filter Text Text [Iterator] deriving Show
 
 runQuery :: (Show a, Filterable a) => Iterator -> [a] -> [a]
-runQuery (Filter field value iterators) tuples = filter (recordsMatch field value) runQuery tuples iterators
+runQuery (Filter field value iterators) tuples = filter (recordsMatch field value) (foldr runQuery tuples iterators)
 
 recordsMatch :: Filterable a => Text -> Text -> a -> Bool
 recordsMatch field value record = filterByField record field == value
 
 queryParser :: [Text] -> Iterator
-queryParser ("filter:":filter:value:rest) = Filter filter value [queryParser rest]
-queryParser ("join:":join1:join2:col1:col2:on_value) = Join join1 join2 col1 col2 on_value
+queryParser ("filter:":filter:value:rest) = Filter filter value []
 
 class Filterable a where
   filterByField :: a -> Text -> Text
+
