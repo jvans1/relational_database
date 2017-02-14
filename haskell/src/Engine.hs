95707a4fe2829e8a12126d1da5d776ad2dc60851
James Vanneman <jvannem@gmail.com>
2017-02-14 14:36:51 -0800

    WIP

--- haskell/src/Engine.hs
+++ haskell/src/Engine.hs
@@ -7,36 +7,36 @@ import Data.Word8(_cr)
 import qualified Data.ByteString.Lazy as BL
 
 data JoinType = CrossProduct
-data Iterator = Filter Int BL.ByteString Iterator | SeqScan String | Join Text Text Text Text JoinType
+data Iterator = Filter Int BL.ByteString Iterator | SeqScan Text | Join Text Text JoinType
 type Row = [BL.ByteString]
 
 executeQuery :: Iterator -> IO [Row]
 executeQuery (Filter num value next)                  = filterBy num value <$> executeQuery next
 executeQuery (SeqScan table)                          = seqScan table
-executeQuery j@(Join _ _ _ _ _)                       = joinTables j
+executeQuery j@Join{}                                 = joinTables j
 
 joinTables :: Iterator -> IO [Row]
-joinTables (Join table1 col1 table2 col2 CrossProduct) = do
-        rows1 <-  seqScan (unpack table1)
-        rows2 <-  seqScan (unpack table1)
+joinTables (Join table1 table2 CrossProduct) = do
+        rows1 <-  seqScan table1
+        rows2 <-  seqScan table1
         return $ do
-          row1 <- (tail rows1)
-          row2 <- (tail rows2)
+          row1 <- rows1
+          row2 <- rows2
           return (row1 ++ row2)
 
 
 filterBy :: Int -> BL.ByteString -> [Row] -> [Row]
 filterBy rowNum rowVal = filter (\row -> length row > rowNum && row !! rowNum == rowVal)
 
-seqScan :: FilePath -> IO [Row]
+seqScan :: Text -> IO [Row]
 seqScan filepath = splitRows <$> scanFile filepath
 
 splitRows :: BL.ByteString -> [Row]
 splitRows = fmap (BL.split 0) . BL.split _cr
 
-scanFile :: FilePath -> IO BL.ByteString
+scanFile :: Text -> IO BL.ByteString
 scanFile filepath = do
-    handle <- openFile filepath ReadMode
+    handle <- openFile (unpack filepath) ReadMode
     hSetBinaryMode handle True
     BL.hGetContents handle
 
@@ -44,5 +44,5 @@ tread = read . unpack
 tToBl= BL.fromStrict . encodeUtf8
 
 parsePlan :: [Text] -> Iterator
-parsePlan ("table":table:"filter":rowNum:value:[]) = Filter (tread rowNum) (tToBl value) (SeqScan $ unpack table)
-parsePlan ("join":table1:col1:table2:col2:_)      = Join table1 col1 table2 col2 CrossProduct
+parsePlan ("table":table:"filter":rowNum:value:[]) = Filter (tread rowNum) (tToBl value) (SeqScan table)
+parsePlan ("join":table1:col1:table2:col2:_)      = Join table1 table2 CrossProduct
