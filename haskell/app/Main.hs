396ef8ff147f0281657b30d158583a2311aea99b
James Vanneman <jvannem@gmail.com>
2017-02-14 14:32:50 -0800

    Executing cross product

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -2,13 +2,16 @@
 module Main where
 import Data.Text(pack)
 import Control.Monad(forM_, mapM_)
+import Data.Text.Encoding(encodeUtf8)
 import Engine(executeQuery, parsePlan)
 import System.Environment(getArgs)
 import qualified Data.ByteString.Lazy as BL
 
+tToBl= BL.fromStrict . encodeUtf8
 main :: IO ()
 main = do
   plan <- parsePlan . fmap pack <$> getArgs
   tuples <- executeQuery plan
-  forM_ tuples $ \tuple -> do
+  forM_ (tail tuples) $ \tuple -> do
+    putStrLn "\n"
     mapM_ BL.putStr tuple
--- haskell/src/Engine.hs
+++ haskell/src/Engine.hs
@@ -6,12 +6,24 @@ import Data.Text.Encoding(encodeUtf8)
 import Data.Word8(_cr)
 import qualified Data.ByteString.Lazy as BL
 
-data Iterator = Filter Int BL.ByteString Iterator | SeqScan String
+data JoinType = CrossProduct
+data Iterator = Filter Int BL.ByteString Iterator | SeqScan String | Join Text Text Text Text JoinType
 type Row = [BL.ByteString]
 
 executeQuery :: Iterator -> IO [Row]
-executeQuery (Filter num value next) = filterBy num value <$> executeQuery next
-executeQuery (SeqScan table)         = seqScan table
+executeQuery (Filter num value next)                  = filterBy num value <$> executeQuery next
+executeQuery (SeqScan table)                          = seqScan table
+executeQuery j@(Join _ _ _ _ _)                       = joinTables j
+
+joinTables :: Iterator -> IO [Row]
+joinTables (Join table1 col1 table2 col2 CrossProduct) = do
+        rows1 <-  seqScan (unpack table1)
+        rows2 <-  seqScan (unpack table1)
+        return $ do
+          row1 <- (tail rows1)
+          row2 <- (tail rows2)
+          return (row1 ++ row2)
+
 
 filterBy :: Int -> BL.ByteString -> [Row] -> [Row]
 filterBy rowNum rowVal = filter (\row -> length row > rowNum && row !! rowNum == rowVal)
@@ -33,3 +45,4 @@ tToBl= BL.fromStrict . encodeUtf8
 
 parsePlan :: [Text] -> Iterator
 parsePlan ("table":table:"filter":rowNum:value:[]) = Filter (tread rowNum) (tToBl value) (SeqScan $ unpack table)
+parsePlan ("join":table1:col1:table2:col2:_)      = Join table1 col1 table2 col2 CrossProduct
